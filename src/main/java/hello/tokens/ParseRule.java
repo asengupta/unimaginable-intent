a1d5a338dc83074a09531891fff8d198375e3a60
asengupta <asengupta@barclaycardus.com>
2016-12-06 15:51:52 -0500

    Starting to write parse rules - Mojo

---
+++ src/main/java/hello/tokens/ParseRule.java
@@ -0,0 +1,14 @@
+package hello.tokens;
+
+/**
+ * Created by asengupta on 12/6/16.
+ */
+public class ParseRule {
+    public ParseRule or(ParseRule expression) {
+        return null;
+    }
+
+    public ParseRule then(ParseRule expression) {
+        return null;
+    }
+}
--- src/test/java/TokenisingTest.java
+++ src/test/java/TokenisingTest.java
@@ -2,6 +2,7 @@ import hello.tokens.Atom;
 import hello.tokens.AtomFactory;
 import hello.pipeline.TokenEmitter;
 import hello.tokenRules.TokenRule;
+import hello.tokens.ParseRule;
 import org.junit.Test;
 
 import java.util.List;
@@ -12,12 +13,49 @@ import java.util.List;
 public class TokenisingTest {
     @Test
     public void canPredictMoveRule() {
-        String sample = " x = (1   +   2)   ;";
+        String sample = " 3+(1   +   2)   ;";
         List<TokenRule> tokens = new TokenEmitter().run(sample);
         System.out.println(tokens);
         List<Atom> atoms = new AtomFactory().build(tokens);
         atoms = new AtomFactory().clean(atoms);
         System.out.println(atoms);
+        expression();
+    }
+
+    private ParseRule expression() {
+        return parenthesizedExpression().or(baldExpression());
+    }
+
+    private ParseRule baldExpression() {
+        return unitaryExpression().or(binaryExpression());
+    }
+
+    private ParseRule binaryExpression() {
+        return expression().then(operator()).then(expression());
+    }
+
+    private ParseRule unitaryExpression() {
+        return number();
+    }
+
+    private ParseRule number() {
+        return null;
+    }
+
+    private ParseRule operator() {
+        return null;
+    }
+
+    private ParseRule parenthesizedExpression() {
+        return openingParenthesis().then(expression()).then(closingParenthesis());
+    }
+
+    private ParseRule closingParenthesis() {
+        return null;
+    }
+
+    private ParseRule openingParenthesis() {
+        return null;
     }
 }
 
